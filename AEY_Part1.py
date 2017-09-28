0a4e5f0919a76d3abed08084b132ef61514b1933
ayamasaki2011 <ayamasak@nd.edu>
2017-09-28 18:37:06 -0400

    Modify script to calculate effect of graduating college on minium wage

--- AEY_Part1.py
+++ AEY_Part1.py
@@ -7,3 +7,20 @@ gender_Experience=wages[["gender","yearsExperience"]]
 ordered_gender_Experience=gender_Experience.sort(['gender','yearsExperience'], ascending=[True,True])
 
 unique_combos=ordered_gender_Experience.drop_duplicates()
+
+print("Unique combinations of gender/years experience:", unique_combos)
+
+wages_highschool = wages[wages.yearsSchool == 12]
+
+wages_college = wages[wages.yearsSchool == 16]
+
+ordered_wages_HS = wages_highschool.sort(columns='wage', ascending=True)
+
+ordered_wages_C = wages_college.sort(columns='wage', ascending=True)
+
+minWage_HS = ordered_wages_HS.iloc[0,3]
+
+minWage_C = ordered_wages_C.iloc[0,3]
+
+print("Effect of graduating college on minimum wage:", minWage_C - minWage_HS)
+
