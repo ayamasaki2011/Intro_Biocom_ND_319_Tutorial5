70caa3d354b3089dd07534ecf2cc1b805975ad1e
ayamasaki2011 <ayamasak@nd.edu>
2017-09-28 18:19:34 -0400

    Modify script to sort gender and years experience first by gender, then by experience

--- AEY_Part1.py
+++ AEY_Part1.py
@@ -4,3 +4,6 @@ wages = pandas.read_csv("wages.csv")
 
 gender_Experience=wages[["gender","yearsExperience"]]
 
+ordered_gender_Experience=gender_Experience.sort(['gender','yearsExperience'], ascending=[True,True])
+
+unique_combos=ordered_gender_Experience.drop_duplicates(inplace=True)
